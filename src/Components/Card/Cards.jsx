6a64825b41b6b0c63bfc4434a9a832eb508ecc19
millamzy <ndefoago@gmail.com>
2024-06-01 18:37:50 +0100

    created productDetails

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom';
-// import CardCss from '../Card/Cards.module.css'
-
-// const Cards = ({ id, title, category, price, discountPercentage, imageUrl }) => {
-//   return (
-//     <Link to={`/product/${id}`} className={CardCss.card}>
-//     <div className={CardCss.movcard}>
-//         <img src={props.imageUrl}/>     
-//         <h5>{props.title}</h5>
-//             <p>{props.category}</p>
-//         <div className={CardCss.price}>
-//            <h5> ${props.price}<span className={CardCss.price2}> ${props.discountPercentage}</span></h5>
-//         </div>
-//     </div>
-//     </Link>
-//   )
-// }
-
-// export default Cards
 
 
 
@@ -26,6 +6,9 @@ import { Link } from 'react-router-dom';
 import CardCss from './Cards.module.css';
 
 const Cards = ({ id, title, category, price, discountPercentage, imageUrl }) => {
+
+ 
+
   return (
     <Link to={`/product/${id}`} className={CardCss.card}>
       <div className={CardCss.movcard}>
