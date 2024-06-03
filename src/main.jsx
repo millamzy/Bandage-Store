c495b82a0856378c4527aae96181020175334b10
millamzy <ndefoago@gmail.com>
2024-06-03 21:07:59 +0100

    resulving error

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,8 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { Provider } from 'react-redux';
-// import { store } from './Redux/store.jsx';
-
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-
-//     <Provider store={store}> 
-//       <App />
-//       </Provider>
-   
-//   </React.StrictMode>,
-// )
-
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import Store from './Redux/Store'; // Ensure this path is correct
+import Store from './Redux/Store.jsx'; 
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
